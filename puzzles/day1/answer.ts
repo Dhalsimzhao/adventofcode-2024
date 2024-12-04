e1d11ba1da07a973b6c752dc13637c26281187a9
zhaolu <zhaolu880226@163.com>
2024-12-04 11:02:40 +0800

    day1

---
+++ puzzles/day1/answer.ts
@@ -0,0 +1,47 @@
+type Input = string
+
+function parser (input: Input) {
+  const rows: number[][] = input.trim().split('\n').map(row=> row.split(/\s+/).map(s => +s))
+  const list1 = [], list2 = []
+  rows.forEach(row => {
+    list1.push(row[0])
+    list2.push(row[1])
+  })
+  return [list1, list2]
+}
+
+function sort(list: number[]) {
+  return list.toSorted((a, b) => a - b)
+}
+
+export function solution1 (input: Input) {
+  let [list1, list2] = parser(input)
+  list1 = sort(list1)
+  list2 = sort(list2)
+
+  return list1.reduce((acc, cur, index) => {
+    return acc + Math.abs(list1[index] - list2[index])
+  }, 0)
+}
+
+export function solution2(input: Input) {
+  let [list1, list2] = parser(input)
+  const memo = new Map()
+  let result = 0
+  list1.forEach(e => {
+    if(!memo.has(e)) {
+      const count = list2.reduce((acc, cur) => {
+        if(cur === e) {
+          acc++
+        }
+        return acc
+      }, 0)
+      memo.set(e, count)
+    }
+    const count = memo.get(e)
+
+    result += e * count
+  })
+
+  return result
+}
---
+++ puzzles/day1/test.ts
@@ -0,0 +1,5 @@
+import input from './input'
+import { solution1, solution2} from './answer'
+
+console.log('solution1', solution1(input))
+console.log('solution2', solution2(input))
